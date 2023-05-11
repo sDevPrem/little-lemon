b0d89f1713d8afc24e91633b9a1b943d513bf04f
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-11 16:06:22 +0530

    Create Onboard screen

--- LittleLemon/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ LittleLemon/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -11,18 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import com.example.littlelemon.ui.theme.app.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LittleLemonTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    OnBoarding()
                 }
             }
         }
