c4991403d5af2b87e68aee665d3a3607170a6c33
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-12 13:46:12 +0530

    Create home header

--- LittleLemon/app/src/main/java/com/example/littlelemon/Home.kt
+++ LittleLemon/app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,25 +1,42 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "Home",
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "little lemon logo",
             modifier = Modifier
+                .padding(top = 16.dp, bottom = 16.dp, start = 50.dp)
+                .height(50.dp)
+                .weight(1F, true)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "profile",
+            modifier = Modifier
+                .padding(vertical = 16.dp, horizontal = 16.dp)
+                .size(50.dp)
                 .clickable {
                     navController.navigate(Destinations.Profile.getRoute())
                 }
