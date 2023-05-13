71a7fe55e343e2482de0c5a2bf4c80bb2f07788c
Prem Thakur <sdevpremthakur@gmail.com>
2023-05-13 19:01:31 +0530

    Create Home Screen UI

--- LittleLemon/app/src/main/java/com/example/littlelemon/viewmodel/HomeVM.kt
+++ LittleLemon/app/src/main/java/com/example/littlelemon/viewmodel/HomeVM.kt
@@ -3,19 +3,32 @@ package com.example.littlelemon.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.littlelemon.data.AppRepository
+import com.example.littlelemon.data.model.MenuItem
 import com.example.littlelemon.data.model.util.Result
 import com.example.littlelemon.data.model.util.asResult
 import com.example.littlelemon.data.network.LittleLemonApi
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
 class HomeVM : ViewModel() {
     private val appRepository = AppRepository(LittleLemonApi())
-    val menuData = appRepository.getMenuData()
-        .asResult()
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5_000),
-            Result.Loading
-        )
+
+    private val _menuData = MutableStateFlow<Result<List<MenuItem>>>(Result.Loading)
+    val menuData: StateFlow<Result<List<MenuItem>>> = _menuData
+
+    init {
+        fetchData()
+    }
+
+    fun fetchData() {
+        appRepository.getMenuData()
+            .map { it.menu }
+            .asResult()
+            .onEach { _menuData.value = it }
+            .launchIn(viewModelScope)
+    }
+
 }
